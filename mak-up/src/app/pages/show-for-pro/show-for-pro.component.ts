b080332493267a791b4fd1d6e0ee144eb2873ce8
CarmenMunity <49306479+CarmenMunity@users.noreply.github.com>
2019-06-14 13:53:13 +0200

    cosis

--- mak-up/src/app/pages/show-for-pro/show-for-pro.component.ts
+++ mak-up/src/app/pages/show-for-pro/show-for-pro.component.ts
@@ -4,6 +4,7 @@ import { ApiService } from 'src/app/api.service';
 import { Producto } from 'src/app/models/producto.model';
 import { Categoria } from 'src/app/models/categoria.model';
 import { Entrada } from 'src/app/models/entrada.model';
+import { Usuario } from 'src/app/models/usuario.model';
 
 @Component({
   selector: 'app-show-for-pro',
@@ -18,6 +19,8 @@ export class ShowForProComponent implements OnInit {
   entradas: Entrada[];
   producto: Producto;
   cargado: boolean = false;
+  usuarios: Usuario[];
+  usuario: Usuario;
   
   constructor(
     public route: ActivatedRoute,
@@ -46,5 +49,16 @@ export class ShowForProComponent implements OnInit {
     this.apiService.readProducto().subscribe((productos: Producto[]) => {
       this.productos = productos;
     });
+    this.apiService.readUsuario().subscribe((usuarios: Usuario[]) => {
+      this.usuarios = usuarios;
+    });
+  }
+  buscarUsuario(id: number){
+    this.usuarios.forEach((usuario) => {
+      if (usuario["Id"] == id) {
+        this.usuario = usuario;
+      }
+    });
+
   }
 }
